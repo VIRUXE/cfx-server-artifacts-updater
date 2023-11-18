53751e2f98e891bbdbeacb03b5bd49ef5966b5a5
Flávio Pereira <1616657+VIRUXE@users.noreply.github.com>
2023-11-18 14:15:43 +0000

    Add files via upload

--- update-artifacts.ps1
+++ update-artifacts.ps1
@@ -11,24 +11,24 @@ $ProgressPreference         = 'SilentlyContinue'
 $version = if ($args.Count -gt 0) { $args[0] } else { "latest" }
 
 try {
-    $data = Invoke-WebRequest -Uri "https://changelogs-live.fivem.net/api/changelog/versions/win32/server" -UseBasicParsing
+	$data = Invoke-WebRequest -Uri "https://changelogs-live.fivem.net/api/changelog/versions/win32/server" -UseBasicParsing
 } catch {
-    Write-Host "Error: Unable to download changelog data."
-    exit
+	Write-Host "Error: Unable to download changelog data."
+	exit
 }
 
-$changelog     = $data.Content | ConvertFrom-Json
+$changelog = $data.Content | ConvertFrom-Json
 $versionNumber = $changelog.$version
 
 if (-not $versionNumber) { 
-    Write-Host "Unknown version. Try 'recommended'/'optional'/'latest'/'critical' instead." 
-    exit
+	Write-Host "Unknown version. Try 'recommended'/'optional'/'latest'/'critical' instead." 
+	exit
 }
 
 $downloadURL = $changelog."${version}_download"
 if (-not $downloadURL) { 
-    Write-Host "Unable to get download URL." 
-    exit
+	Write-Host "Unable to get download URL." 
+	exit
 }
 
 $txAdmin = $changelog."${version}_txadmin"
@@ -37,33 +37,37 @@ if (-not $txAdmin) { $txAdmin = "Unknown" }
 Write-Host "Downloading '$version' artifacts, version '$versionNumber' (txAdmin '$txAdmin')... "
 
 try {
-    $file = Invoke-WebRequest -Uri $downloadURL -UseBasicParsing
+	$file = Invoke-WebRequest -Uri $downloadURL -UseBasicParsing
 } catch {
-    Write-Host "Error: Unable to download the file."
-    exit
+	Write-Host "Error: Unable to download the file."
+	exit
 }
 
 $fileName = [System.IO.Path]::GetFileName($downloadURL)
 
 if (-not $file) {
-    Write-Host "Error: Download failed or file is empty."
-    exit
+	Write-Host "Error: Download failed or file is empty."
+	exit
 }
 
 Write-Host "Done.`nSaving... "
 [System.IO.File]::WriteAllBytes($fileName, $file.Content)
 Write-Host "Done."
 
-Write-Host "Opening Archive... "
 if (Test-Path $fileName) {
-    Write-Host "Done.`nExtracting... "
-    $extractPath = "./artifacts/"
-    New-Item -ItemType Directory -Force -Path $extractPath | Out-Null
-    Expand-Archive -LiteralPath $fileName -DestinationPath $extractPath -Force
-    Write-Host "Done."
+	Write-Host "Extracting... "
+	$extractPath = "./artifacts/"
+
+	if (-not (Test-Path $extractPath)) {
+		Write-Host "Creating artifacts directory..."
+		New-Item -ItemType Directory -Force -Path $extractPath | Out-Null
+	}
+
+	Expand-Archive -LiteralPath $fileName -DestinationPath $extractPath -Force
+	Write-Host "Extraction complete."
 } else {
-    Write-Host "Error: Downloaded file is missing or inaccessible."
-    exit
+	Write-Host "Error: Downloaded file is missing or inaccessible."
+	exit
 }
 Remove-Item $fileName
 
