ac8884a370feaecc7fa675291acaf9394f7aa844
Flávio Pereira <1616657+VIRUXE@users.noreply.github.com>
2024-03-24 12:44:38 +0000

    fixed file path for powershell script

--- update-artifacts.ps1
+++ update-artifacts.ps1
@@ -6,70 +6,74 @@
 #>
 
 $originalProgressPreference = $ProgressPreference
-$ProgressPreference         = 'SilentlyContinue'
+$ProgressPreference = 'SilentlyContinue'
 
-$version = if ($args.Count -gt 0) { $args[0] } else { "latest" }
+$version = $args.Count -gt 0 ? $args[0] : "latest"
 
 try {
-	$data = Invoke-WebRequest -Uri "https://changelogs-live.fivem.net/api/changelog/versions/win32/server" -UseBasicParsing
+    $data = Invoke-WebRequest -Uri "https://changelogs-live.fivem.net/api/changelog/versions/win32/server" -UseBasicParsing
 } catch {
-	Write-Host "Error: Unable to download changelog data."
-	exit
+    Write-Host "Error: Unable to download changelog data."
+    exit
 }
 
 $changelog = $data.Content | ConvertFrom-Json
 $versionNumber = $changelog.$version
 
 if (-not $versionNumber) { 
-	Write-Host "Unknown version. Try 'recommended'/'optional'/'latest'/'critical' instead." 
-	exit
+    Write-Host "Unknown version. Try 'recommended'/'optional'/'latest'/'critical' instead." 
+    exit
 }
 
 $downloadURL = $changelog."${version}_download"
 if (-not $downloadURL) { 
-	Write-Host "Unable to get download URL." 
-	exit
+    Write-Host "Unable to get download URL." 
+    exit
 }
 
-$txAdmin = $changelog."${version}_txadmin"
-if (-not $txAdmin) { $txAdmin = "Unknown" }
+$txAdmin = $changelog."${version}_txadmin" -ne $null ? $changelog."${version}_txadmin" : "Unknown"
 
 Write-Host "Downloading '$version' artifacts, version '$versionNumber' (txAdmin '$txAdmin')... "
 
 try {
-	$file = Invoke-WebRequest -Uri $downloadURL -UseBasicParsing
+    $file = Invoke-WebRequest -Uri $downloadURL -UseBasicParsing
 } catch {
-	Write-Host "Error: Unable to download the file."
-	exit
+    Write-Host "Error: Unable to download the file."
+    exit
 }
 
-$fileName = [System.IO.Path]::GetFileName($downloadURL)
-
 if (-not $file) {
-	Write-Host "Error: Download failed or file is empty."
-	exit
+    Write-Host "Error: Download failed or file is empty."
+    exit
 }
 
 Write-Host "Done.`nSaving... "
-[System.IO.File]::WriteAllBytes($fileName, $file.Content)
-Write-Host "Done."
-
-if (Test-Path $fileName) {
-	Write-Host "Extracting... "
-	$extractPath = "./artifacts/"
-
-	if (-not (Test-Path $extractPath)) {
-		Write-Host "Creating artifacts directory..."
-		New-Item -ItemType Directory -Force -Path $extractPath | Out-Null
-	}
-
-	Expand-Archive -LiteralPath $fileName -DestinationPath $extractPath -Force
-	Write-Host "Extraction complete."
+if ($file.Content.Length -gt 0) {
+    $filePath = Join-Path -Path (Get-Location) -ChildPath "server.zip"
+    [System.IO.File]::WriteAllBytes($filePath, $file.Content)
+    Write-Host "File saved to $filePath"
 } else {
-	Write-Host "Error: Downloaded file is missing or inaccessible."
-	exit
+    Write-Host "Error: File downloaded is empty."
+    exit
 }
-Remove-Item $fileName
+
+$filePath = Join-Path -Path (Get-Location) -ChildPath "server.zip"
+if (Test-Path $filePath) {
+    Write-Host "Extracting... "
+    $extractPath = Join-Path -Path (Get-Location) -ChildPath "artifacts\"
+
+    if (-not (Test-Path $extractPath)) {
+        Write-Host "Creating artifacts directory..."
+        New-Item -ItemType Directory -Force -Path $extractPath | Out-Null
+    }
+
+    Expand-Archive -LiteralPath $filePath -DestinationPath $extractPath -Force
+    Write-Host "Extraction complete."
+    Remove-Item $filePath -Force
+} else {
+    Write-Host "Error: Downloaded file is missing or inaccessible."
+    exit
+}
 
 $ProgressPreference = $originalProgressPreference
 
