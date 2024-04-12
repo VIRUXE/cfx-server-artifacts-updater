36a65b7ecea2a478e9abc8543dbd3a35c0a5ad3f
VIRUXE <1616657+VIRUXE@users.noreply.github.com>
2024-04-12 13:17:43 +0100

    tweak: don't delete fxserver archive on script end

--- artifacts.php
+++ artifacts.php
@@ -1,6 +1,8 @@
 <?php
 /*
 	Made by http://www.github.com/viruxe
+
+	Requirements: openssl and zip extensions
 	
 	Windows - an "artifacts" folder gets created
 	Linux - will just extract the files onto the current working folder
@@ -8,31 +10,29 @@
 	Usage: artifacts.php ('recommended'/'optional'/'latest'/'critical')
 	The latest version will be downloaded if no argument is provided.
 */
-$os      = strtoupper(substr(PHP_OS, 0, 3)) === 'WIN' ? "win32" : "linux";  // Detect Operating System to select which archive we should get
-$version = $argv[1] ?? "latest";
+$os = PHP_OS_FAMILY === 'Windows' ? 'win32' : 'linux';
 
 print("Downloading Changelog for '$os'... ");
 
-$data = file_get_contents("https://changelogs-live.fivem.net/api/changelog/versions/$os/server");
+$changelogManifestJson = file_get_contents("https://changelogs-live.fivem.net/api/changelog/versions/$os/server");
+	
+if($changelogManifestJson) print("Done.\n"); else die("Unable to Download.");
 
-if ($data)
-	print("Done.\n");
-else
-	die("Unable to Download.");
-
-$changelog = json_decode($data, true);
-
-$versionNumber = $changelog[$version] ?? die("Unknown version. Try 'recommended'/'optional'/'latest'/'critical' instead.");
-$downloadURL   = $changelog["{$version}_download"] ?? die("Unable to get download URL.");
-$txAdmin       = $changelog["{$version}_txadmin"] ?? "Unknown";
+$changelog = json_decode($changelogManifestJson, true);
+	
+$artifactsType = $argv[1] ?? "latest";
+$versionNumber = $changelog[$artifactsType] ?? die("Unknown version. Try 'recommended'/'optional'/'latest'/'critical' instead.");
+$downloadURL   = $changelog["{$artifactsType}_download"] ?? die("Unable to get download URL.");
+$txAdmin       = $changelog["{$artifactsType}_txadmin"] ?? "Unknown";
 $fileName      = basename($downloadURL);
 
-print("Downloading '$version' artifacts, version '$versionNumber' (txAdmin '$txAdmin')... ");
+print("Downloading '$artifactsType' artifacts, version '$versionNumber' (txAdmin '$txAdmin')... ");
 
-$file = file_get_contents($downloadURL);
-if ($file) {
+$fileContents = file_get_contents($downloadURL);
+
+if($fileContents) {
 	print("Done.\nSaving... ");
-	file_put_contents($fileName, $file);
+	file_put_contents($fileName, $fileContents);
 	print("Done.\n");
 } else
 	die("Unable to Download.");
@@ -41,7 +41,7 @@ if($os === "win32") {
 	$archive = new ZipArchive;
 	
 	print("Opening Archive... ");
-	if ($archive->open($fileName)) {
+	if($archive->open($fileName)) {
 		print("Done.\nExtracting... ");
 		print(@$archive->extractTo("./artifacts/") ? "Done." : "Unable to extract. Is the server running?") . PHP_EOL;
 		$archive->close();
@@ -49,7 +49,6 @@ if($os === "win32") {
 		die("Unable to open.");
 } else {
 	$command = "tar xfJ $fileName";
-	print("Executed '$command'. Verify the current working directory.\n");
 	shell_exec($command);
+	print("Executed '$command'. Verify the current working directory.\n");
 }
-unlink($fileName);
